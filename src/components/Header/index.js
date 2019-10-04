a51cb20353684b67d29d65ff9997fd0202ed599a
Rodrigo Matos <rodrigocostamatos2211@gmail.com>
2019-10-04 21:13:29 +0100

    updates 1

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import { Container } from './styles';
+import logo from '../../assets/images/logo.svg';
 
 export default function Header() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Link to="/">
+        <img src={logo} alt="Rocketseat" />
+      </Link>
+      {/* <Cart to="/cart"></Cart> */}
+    </Container>
+  );
 }
