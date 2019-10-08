06fe9bc6e73e38ec520f4b39d17a6216ff730534
Rodrigo Matos <rodrigocostamatos2211@gmail.com>
2019-10-08 17:43:30 +0100

    add formating

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { ProductList } from './styles';
 import { MdAddShoppingCart } from 'react-icons/md';
 import api from '../../server/api';
+import { formatPrice } from '../../util/format';
 
 export default class Home extends Component {
   state = {
@@ -10,9 +11,15 @@ export default class Home extends Component {
   };
 
   async componentDidMount() {
-    // const response = await api.get('products');
-    // this.setState({ products: response.data });
-    // console.log(response.data, 'teste');
+    const response = await api.get('products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormated: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+    console.log(response.data, 'teste');
   }
 
   render() {
@@ -21,13 +28,10 @@ export default class Home extends Component {
     return (
       <ProductList>
         {products.map(product => (
-          <li>
-            <img
-              src="https://static.netshoes.com.br/produtos/chuteira-futsal-joma-vulcano-in/14/049-3391-014/049-3391-014_detalhe1.jpg?ims=326x"
-              alt="Tenis"
-            ></img>
-            <strong>Tênis para seu treino</strong>
-            <span>R$ 130,00</span>
+          <li key={product.id}>
+            <img src={product.image} alt={product.title}></img>
+            <strong>{product.title}</strong>
+            <span>{product.priceFormated}</span>
 
             <button type="button">
               <div>
